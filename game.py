0655ca8de81984d4706f893a8fee7c8d562be109
tormobr <tormobr@student.matnat.uio.no>
2019-01-14 20:51:29 +0100

    added pipes to the game

--- game.py
+++ game.py
@@ -1,16 +1,21 @@
 import os
 import pygame
 from bird import Bird
+from pipe import Pipe
 
 WHITE = (255,255,255)
 LIGHT_BLUE = (135, 200, 250)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 G = 1
+SCREEN_WIDTH = 450
+SCREEN_HEIGHT = 600
 class Game:
     
     def __init__(self):
         self.player = Bird(100,100, 10, 10)
         self.speed = 0
+        self.pipes = [Pipe(300, 120, SCREEN_HEIGHT), Pipe(500, 120, SCREEN_HEIGHT), Pipe(700, 120, SCREEN_HEIGHT)]
 
     def play(self):
         print("STARTING GAME!!")
@@ -20,7 +25,7 @@ class Game:
 
         # Set up the display
         pygame.display.set_caption("FLAPPY-BIRD")
-        screen = pygame.display.set_mode((570, 660))
+        screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
         clock = pygame.time.Clock()
 
@@ -41,13 +46,27 @@ class Game:
                     if e.key == pygame.K_SPACE:
                         self.speed = -10
 
+
+
             self.speed += G*0.4
             self.player.move(self.speed)
 
+            for pipe in self.pipes:
+                pipe.move(3)
+
+            for pipe in self.pipes:
+                if pipe.top_rect.x + 70 < 0:
+                    self.pipes[0] = self.pipes[1]
+                    self.pipes[1] = self.pipes[2]
+                    self.pipes[2] = Pipe(self.pipes[1].top_rect.x+200, 100, SCREEN_HEIGHT)
 
             screen.fill(LIGHT_BLUE)
 
             pygame.draw.rect(screen, RED, self.player.rect)
+
+            for pipe in self.pipes:
+                pygame.draw.rect(screen, GREEN, pipe.top_rect)
+                pygame.draw.rect(screen, GREEN, pipe.bottom_rect)
             pygame.display.flip()
 
 
--- pipe.py
+++ pipe.py
@@ -1,3 +1,12 @@
+import random
+import pygame
 class Pipe:
-	def __init__(x ,y):
-		self.rect = pygame.Rect(x,y,100,100)
+	def __init__(self, x, space, height):
+		self.top_rect = pygame.Rect(x,0,70, random.randrange(height/4, height*3/4))
+
+		self.bottom_rect = pygame.Rect(x, self.top_rect.height+space, 70, 400)
+
+	def move(self, speed):
+		self.top_rect.x -= speed
+		self.bottom_rect.x -= speed
+
